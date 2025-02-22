ae6d72e55a0f245a4c1ee8c36b98f1c01908fca6
Gabriel Marinho <gabriel.anjo722@gmail.com>
2025-02-21 22:34:54 -0300

    feat: start getUsers service

--- app/components/Sidebar.tsx
+++ app/components/Sidebar.tsx
@@ -1,34 +1,53 @@
-"use client"
+"use client";
 
-import Image from 'next/image'
-import React, { useState } from 'react'
-import styles from "@/app/styles/components/sidebar.module.scss"
+import Image from "next/image";
+import React, { useEffect, useState } from "react";
+import styles from "@/app/styles/components/sidebar.module.scss";
+import getUsers from "../services/getUsers";
 
 const Sidebar: React.FC = () => {
+    const [isOpen, setIsOpen] = useState(false);
 
-  const [isOpen, setIsOpen] = useState(false)
-  
-  return (
-   <>
-   
-   {/* Sidebar */}
-    <div className={`${styles.sidebar} ${isOpen ? 'open' : ''}`}>
-    <ul>
-      <li><a href="#">Home</a></li>
-      <li><a href="#">Home</a></li>
-      <li><a href="#">Home</a></li>
-      <li><a href="#">Home</a></li>
-      <li><a href="#">Home</a></li>
+    useEffect(() => {
+        if (!localStorage.getItem("users")) {
+            getUsers();
+        }
+    }, []);
 
-    </ul>
-    </div>
+    return (
+        <>
+            {/* Sidebar */}
+            <div className={`${styles.sidebar} ${isOpen ? "open" : ""}`}>
+                <ul>
+                    <li>
+                        <a href="#">Home</a>
+                    </li>
+                    <li>
+                        <a href="#">Home</a>
+                    </li>
+                    <li>
+                        <a href="#">Home</a>
+                    </li>
+                    <li>
+                        <a href="#">Home</a>
+                    </li>
+                    <li>
+                        <a href="#">Home</a>
+                    </li>
+                </ul>
+            </div>
 
-   {/* Hamburger Menu */}
-   <div className='hamburguer'>
-    <Image src="/assets/img/icons/hamburger-menu.webp" width={40} height={40} alt='Hamburger Icon'/>
-   </div>
-   </>
-  )
-}
+            {/* Hamburger Menu */}
+            <div className="hamburguer">
+                <Image
+                    src="/assets/img/icons/hamburger-menu.webp"
+                    width={40}
+                    height={40}
+                    alt="Hamburger Icon"
+                />
+            </div>
+        </>
+    );
+};
 
-export default Sidebar
+export default Sidebar;
